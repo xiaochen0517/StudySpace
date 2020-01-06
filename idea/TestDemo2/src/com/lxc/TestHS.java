d38193c721b899866b476bb08e101ed2c3005a6f
xiaochen0517 <2827075398@qq.com>
2020-01-06 12:04:10 +0800

    request method

---
+++ idea/TestDemo2/src/com/lxc/TestHS.java
@@ -0,0 +1,30 @@
+package com.lxc;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/tesths")
+public class TestHS extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        System.out.println("HttpServlet");
+        System.out.println(req.getMethod());
+        System.out.println(req.getContextPath());
+        System.out.println(req.getServletPath());
+        System.out.println(req.getQueryString());
+        System.out.println(req.getRequestURI());
+        System.out.println(req.getRequestURL());
+        System.out.println(req.getProtocol());
+        System.out.println(req.getRemoteAddr());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
